7face7c725996ace1e7ca19beecfd01c51fa27fe
Bob Yuan <bob.yuan@yahoo.com>
2020-04-29 21:45:57 -0700

    Add db migration

--- src/EnterpriseSolutions.IdentityService/Startup.cs
+++ src/EnterpriseSolutions.IdentityService/Startup.cs
@@ -2,10 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EnterpriseSolutions.IdentityService.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,6 +23,16 @@ namespace EnterpriseSolutions.IdentityService
         }
 
         public IConfiguration Configuration { get; }
+
+        protected virtual void AddDbContext(IServiceCollection services)
+        {
+            services.AddDbContext<AppIdentityDbContext>(ConfigureDatabase);
+        }
+
+        protected virtual void ConfigureDatabase(DbContextOptionsBuilder ctxBuilder)
+        {
+            ctxBuilder.UseSqlServer(Configuration.GetConnectionString("Default"));
+        }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
