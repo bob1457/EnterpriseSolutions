d163472c019eff95864ac0ae59a217fec9c9a8e3
Bob Yuan <bob.yuan@yahoo.com>
2020-04-29 21:26:39 -0700

    Initial setup

---
+++ src/EnterpriseSolutions.IdentityService/Data/AppIdentityDbContextFactory.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EnterpriseSolutions.IdentityService.Data
+{
+    public class AppIdentityDbContextFactory : DesignTimeDbContextFactoryBase<AppIdentityDbContext>
+    {
+        protected override AppIdentityDbContext CreateNewInstance(DbContextOptions<AppIdentityDbContext> options)
+        {
+            //throw new NotImplementedException();
+            return new AppIdentityDbContext(options);
+        }
+    }
+}
---
+++ src/EnterpriseSolutions.IdentityService/Data/PersistedGrantDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+
+namespace EnterpriseSolutions.IdentityService.Data
+{
+    public class PersistedGrantDbContextFactory : DesignTimeDbContextFactoryBase<PersistedGrantDbContext>
+    {
+        protected override PersistedGrantDbContext CreateNewInstance(DbContextOptions<PersistedGrantDbContext> options)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<PersistedGrantDbContext>();
+            optionsBuilder.UseSqlServer(ConnectionString, sql => sql.MigrationsAssembly(this.GetType().GetTypeInfo().Assembly.GetName().Name));
+            return new PersistedGrantDbContext(optionsBuilder.Options, new OperationalStoreOptions());
+        }
+    }
+}
