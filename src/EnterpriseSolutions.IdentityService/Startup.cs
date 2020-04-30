49aa4f6a5c2c5c8587987749533fb05f85a56bec
Bob Yuan <bob.yuan@yahoo.com>
2020-04-29 22:11:42 -0700

    Add identityserver config

--- src/EnterpriseSolutions.IdentityService/Startup.cs
+++ src/EnterpriseSolutions.IdentityService/Startup.cs
@@ -3,9 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using EnterpriseSolutions.IdentityService.Data;
+using EnterpriseSolutions.IdentityService.Models;
+using IdentityServer4.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -17,12 +20,15 @@ namespace EnterpriseSolutions.IdentityService
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
         }
 
         public IConfiguration Configuration { get; }
+
+        public IWebHostEnvironment Environment { get; }
 
         protected virtual void AddDbContext(IServiceCollection services)
         {
@@ -37,7 +43,41 @@ namespace EnterpriseSolutions.IdentityService
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            AddDbContext(services);
+
+            services.AddIdentity<AppUser, IdentityRole>()
+            .AddEntityFrameworkStores<AppIdentityDbContext>();
+
+            var builder = services.AddIdentityServer()
+              // this adds the operational data from DB (codes, tokens, consents)
+              .AddOperationalStore(options =>
+              {
+                  options.ConfigureDbContext = ConfigureDatabase;
+                  // this enables automatic token cleanup. this is optional.
+                  options.EnableTokenCleanup = true;
+                  options.TokenCleanupInterval = 30; // interval in seconds
+              })
+              //.AddInMemoryPersistedGrants()
+              .AddInMemoryIdentityResources(Config.GetIdentityResources())
+              .AddInMemoryApiResources(Config.GetApiResources())
+              //.AddInMemoryClients(Config.GetClients(Configuration.GetValue("AppSettings:Address", "")))
+              .AddInMemoryClients(Config.GetClients())
+              .AddAspNetIdentity<AppUser>();
+
+            if (Environment.IsDevelopment())
+            {
+                builder.AddDeveloperSigningCredential();
+            }
+            else
+            {
+                throw new Exception("need to configure key material");
+            }
+
             services.AddControllers();
+
+            //services.AddTransient<IProfileService, IdentityClaimsProfileService>();
+            //services.AddScoped<IUserRepository, UserRepository>();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Latest);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -54,10 +94,17 @@ namespace EnterpriseSolutions.IdentityService
 
             app.UseAuthorization();
 
+            app.UseIdentityServer();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllers();
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapControllers();
+            //});
         }
     }
 }
